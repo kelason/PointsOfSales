cd4b787e3ff9c2ae0c5dfdbdefea48248ca4611e
kelason <kevin.sapang@gmail.com>
2020-12-09 13:40:30 +0800

    added barcode generator function

--- config/functions.php
+++ config/functions.php
@@ -19,3 +19,18 @@ function cors($method) {
         header("Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With");
     }
 }
+// Get 6 string and 6 number randomly
+function genRandStr(){
+    $a = $b = '';
+  
+    for($i = 0; $i < 6; $i++){
+      $a .= chr(mt_rand(65, 90)); // see the ascii table why 65 to 90.    
+      $b .= mt_rand(0, 9);
+    }
+  
+    return $a . $b;
+}
+
+function Space($int) { $spc = '';
+	for ($a = 0; $a <= $int; $a++) { $spc .= ' '; } return $spc;
+}
